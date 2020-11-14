94550796ebf2f8ce7bf7001acc77fd2b3e711ed3
Gustavo Barbosa <gustavo@vortice.inf.br>
2020-11-14 15:46:43 -0300

    Aula de teste de integração: criando as classe base para iniciar os testes

---
+++ Teste de Integracao/03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/Config/IntegrationTestsFixture.cs
@@ -0,0 +1,33 @@
+﻿using NerdStore.WebApp.MVC;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text;
+using Xunit;
+
+namespace NerdStore.WebApp.Tests.Config
+{
+    [CollectionDefinition(nameof(IntegrationWebTestFixtureCollection))]
+    public class IntegrationWebTestFixtureCollection : ICollectionFixture<IntegrationTestsFixture<StartupWebTests>> { }
+
+    [CollectionDefinition(nameof(IntegrationApiTestFixtureCollection))]
+    public class IntegrationApiTestFixtureCollection : ICollectionFixture<IntegrationTestsFixture<StartupApiTests>> { }
+
+    public class IntegrationTestsFixture<TStartup> : IDisposable where TStartup : class
+    {
+        public readonly LojaAppFactory<TStartup> Factory;
+        public HttpClient Client;
+
+        public IntegrationTestsFixture()
+        {
+            Factory = new LojaAppFactory<TStartup>();
+            Client = Factory.CreateClient();
+        }
+
+        public void Dispose()
+        {
+            Client.Dispose();
+            Factory.Dispose();
+        }
+    }
+}
---
+++ Teste de Integracao/03 - Testes de Integracao/tests/NerdStore.WebApp.Tests/UsuarioTests.cs
@@ -0,0 +1,17 @@
+﻿using NerdStore.WebApp.MVC;
+using NerdStore.WebApp.Tests.Config;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NerdStore.WebApp.Tests
+{
+    public class UsuarioTests
+    {
+        private readonly IntegrationTestsFixture<StartupWebTests> _testsFixture;
+        public UsuarioTests(IntegrationTestsFixture<StartupWebTests> testsFixture)
+        {
+            _testsFixture = testsFixture;
+        }
+    }
+}
