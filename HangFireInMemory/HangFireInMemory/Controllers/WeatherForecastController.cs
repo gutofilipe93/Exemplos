444bece3bc7315ff289be0896662ea649d594d23
Gustavo Barbosa <gustavo@idever.com.br>
2022-03-28 19:29:01 -0300

    HangFireInMemory

---
+++ HangFireInMemory/HangFireInMemory/Controllers/WeatherForecastController.cs
@@ -0,0 +1,70 @@
+﻿using Hangfire;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace HangFireInMemory.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IBackgroundJobClient _backgroundJobs;
+        private static CancellationTokenSource _resetCacheToken;
+        
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IBackgroundJobClient backgroundJobs)
+        {
+            _logger = logger;
+            _backgroundJobs = backgroundJobs;
+            _resetCacheToken = new CancellationTokenSource(); 
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _backgroundJobs.Enqueue(() => TestTempo());            
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+            
+        }
+
+        [HttpGet]
+        [Route("reser")]
+        public string Reset()
+        {
+            if (_resetCacheToken != null && !_resetCacheToken.IsCancellationRequested && _resetCacheToken.Token.CanBeCanceled)
+            {
+                _resetCacheToken.Cancel();
+                _resetCacheToken.Dispose();
+            }
+
+            _resetCacheToken = new CancellationTokenSource();
+
+            return "Done";
+        }
+
+        [ApiExplorerSettings(IgnoreApi = true)]
+        public void TestTempo()
+        {
+            Thread.Sleep(TimeSpan.FromSeconds(15));
+            Console.WriteLine("Hello world from Hangfire!");
+        }
+    }
+}
---
+++ HangFireInMemory/HangFireInMemory/Startup.cs
@@ -0,0 +1,70 @@
+using Hangfire;
+using Hangfire.MemoryStorage;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HangFireInMemory
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddHangfire(config =>
+            {
+                config.UseMemoryStorage();
+            });
+            services.AddHangfireServer();
+
+
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "HangFireInMemory", Version = "v1" });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+
+            app.UseHangfireDashboard("/mydashboard");            
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "HangFireInMemory v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
